9780b9df510951130dd1e3d91b1868f2ae4b7b9a
Anton Dimov <GKNedyalkov23@codingburgas.bg>
2024-11-09 20:27:30 +0200

    Fix the previous commit

--- t9am-Green/map.cpp
+++ t9am-Green/map.cpp
@@ -3,7 +3,7 @@
 Map::Map() {
 	mapTexture = LoadTexture("Graphics/map1.png"); //Contains the map texture
 	doorTexture = LoadTexture("Graphics/Door.png"); //Contains the door texture
-	door = { 170, 70, (float)doorTexture.width, (float)doorTexture.height};
+	door = { 170, 70, (float)doorTexture.width, (float)doorTexture.height}; 
 }
 
 void Map::Draw() { //Draws the map with its elements on the screen
@@ -13,7 +13,7 @@ DrawTexture(doorTexture, door.x, door.y, WHITE); //Draws the door on the screen
 
 }
 
-void Map::UpdateDoor(Rectangle CollidingObject)
+void Map::UpdateDoor(Rectangle CollidingObject) //Updates the door texture if it is colliding with the character
 {
 	if (CheckCollisionRecs(door, CollidingObject))
 	{
